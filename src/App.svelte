dd46655d5d216f7c4f1d8f51b4ab4e3b34bcc7a9
Kevin Mason <krmason2020@gmail.com>
2022-05-23 12:01:36 -0700

    onto tutorial 7 now https://www.youtube.com/watch?v=VWOpVxIkZJs&list=PL4cUxeGkcC9hlbrVO_2QFVqVPhlZmz7tO&index=7&ab_channel=TheNetNinja

--- src/App.svelte
+++ src/App.svelte
@@ -1,53 +1,34 @@
 <script>
-	let firstName = "Jimmy"
-	let lastName = "Page";
-	let beltColor = "black";
-
-	$: fullName = `${firstName} ${lastName}`//reactive value
-	// $: console.log(beltColor)//will run any time a value changes
-	// $: console.log(fullName)
-
-	//or you can use a code block rather than 2 $:
-
-	$: {
-		console.log(beltColor)//will run any time a value changes
-		console.log(fullName)
-	}
-
-	const handleClick =()=>{
-		beltColor = "orange"
-	}
-
-	const handleInput = (e)=>{
-		beltColor = e.target.value;
-	}
+  let people = [
+    { name: "yoshi", beltColor: "black", age: 25, id: 1 },
+    { name: "mario", beltColor: "red", age: 33, id: 2 },
+    { name: "luigi", beltColor: "green", age: 29, id: 3 },
+  ];
 </script>
 
 <main>
-	<p style="color:{beltColor}">{fullName} - {beltColor} belt</p>
-	<input type="text" bind:value={firstName}>
-	<input type="text" bind:value={lastName}>
-	<input type="text" bind:value={beltColor}>
+  {#each people as person (person.id)}
+    <!-- person.id is the key -->
+    <div>
+      <h4>{person.name}</h4>
+      <p>{person.age} years old, has a {person.beltColor} belt.</p>
+    </div>
+  {:else}
+    <p>there are no people to show</p>
+  {/each}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
