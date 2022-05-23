69b4076dad31014e715a984156ca40a93a54dbde
Kevin Mason <krmason2020@gmail.com>
2022-05-23 13:15:49 -0700

    onto props https://www.youtube.com/watch?v=Y1McRGLkxQc&list=PL4cUxeGkcC9hlbrVO_2QFVqVPhlZmz7tO&index=11&ab_channel=TheNetNinja

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,11 +1,33 @@
 <script>
   let showModal = true;
+  let isPromo = false;
 </script>
 
 {#if showModal}
   <div class="backdrop">
-    <div class="modal"><p>sign up for offers</p></div>
+    <div class="modal" class:promo={isPromo}><p>sign up for offers</p></div>
+    <!-- the class: adds classes conditionally -->
   </div>
 {/if}
 
-<style></style>
+<style>
+  .backdrop {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+  .modal {
+    padding: 10px;
+    border-radius: 10px;
+    max-width: 400px;
+    margin: 10% auto;
+    text-align: center;
+    background-color: white;
+  }
+
+  .promo.modal {
+    background-color: crimson;
+    color: white;
+  }
+</style>
