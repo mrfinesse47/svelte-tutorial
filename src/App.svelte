573a311e6a3cf70ba075776c5c28716947ddf703
Kevin Mason <krmason2020@gmail.com>
2022-05-23 12:33:17 -0700

    onto tut 8 https://www.youtube.com/watch?v=rkwKpULfWZA&list=PL4cUxeGkcC9hlbrVO_2QFVqVPhlZmz7tO&index=9&ab_channel=TheNetNinja

--- src/App.svelte
+++ src/App.svelte
@@ -9,13 +9,24 @@
     //delete person  from people
     people = people.filter((p) => p.id !== id);
   };
+
+  let num = 5;
 </script>
 
+{#if num > 20}
+  <p>greater than 20</p>
+{:else if num > 5}
+  <p>num is greater than 5</p>
+{:else}
+  <p>num is less than 5</p>
+{/if}
 <main>
   {#each people as person (person.id)}
     <!-- person.id is the key -->
     <div>
       <h4>{person.name}</h4>
+      {#if person.beltColor === "black"}
+        <p><strong>Master Ninja</strong></p>{/if}
       <p>{person.age} years old, has a {person.beltColor} belt.</p>
       <button on:click={() => handleClick(person)}>delete</button>
     </div>
