ceb61f6dbe91d5b06b94ad326c7cff3f3276a7a0
Kevin Mason <krmason2020@gmail.com>
2022-05-23 19:29:12 -0700

    onto https://www.youtube.com/watch?v=yCkYm4zze8I&list=PL4cUxeGkcC9hlbrVO_2QFVqVPhlZmz7tO&index=17&ab_channel=TheNetNinja

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -2,9 +2,14 @@
   let name;
   let beltColor;
   let age;
+  //   let fighting = false;
+  //   let sneaking = false;
+  //   let running = false;
+  let skills = [];
 
   const handleSubmit = () => {
     console.log(`name ${name}, color ${beltColor}, age ${age}`);
+    console.log(skills);
   };
 </script>
 
@@ -12,6 +17,13 @@
   <input type="text" placeholder="name" bind:value={name} />
   <input type="text" placeholder="belt color" bind:value={beltColor} />
   <input type="number" placeholder="age" bind:value={age} />
+  <label>Skills</label>
+  <!-- <input type="checkbox" bind:checked={fighting} />fighting<br />
+  <input type="checkbox" bind:checked={sneaking} />sneaking<br />
+  <input type="checkbox" bind:checked={running} />running<br /> -->
+  <input type="checkbox" bind:group={skills} value="fighting" />fighting<br />
+  <input type="checkbox" bind:group={skills} value="sneaking" />sneaking<br />
+  <input type="checkbox" bind:group={skills} value="running" />running<br />
   <button>Add person</button>
 </form>
 
